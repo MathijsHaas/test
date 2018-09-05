aae03dae25ad9a45a541fe7ba218db676659fc65
Mathijs <m.a.hasebos@gmail.com>
2018-09-05 16:08:10 +0200

    dingetje

--- color_follow.py
+++ color_follow.py
@@ -15,17 +15,28 @@ except ImportError:
             "Failed to import library from parent folder")
 import time
 import random
-import pygame
+from pygame import mixer
+import buttonlayout
 import multiprocessing
 
+button1 = buttonlayout.color_follow_button1
+button2 = buttonlayout.color_follow_button2
+button3 = buttonlayout.color_follow_button3
+button4 = buttonlayout.color_follow_button4
 
-pygame.mixer.init()
-bleep1 = pygame.mixer.Sound("bleep2.ogg")
-bleep2 = pygame.mixer.Sound("bleep2.ogg")
-bleep3 = pygame.mixer.Sound("bleep2.ogg")
-bleep4 = pygame.mixer.Sound("bleep2.ogg")
-wrong_sound = pygame.mixer.Sound("wrong_sound.ogg")
-good_sound = pygame.mixer.Sound("good_sound.ogg")
+led1 = buttonlayout.color_follow_led1
+led2 = buttonlayout.color_follow_led2
+led3 = buttonlayout.color_follow_led3
+led4 = buttonlayout.color_follow_led4
+
+
+mixer.init()
+bleep1 = mixer.Sound("bleep2.ogg")
+bleep2 = mixer.Sound("bleep2.ogg")
+bleep3 = mixer.Sound("bleep2.ogg")
+bleep4 = mixer.Sound("bleep2.ogg")
+wrong_sound = mixer.Sound("wrong_sound.ogg")
+good_sound = mixer.Sound("good_sound.ogg")
 
 # IO PI PLUS shield setup
 
@@ -41,7 +52,7 @@ iobus2.set_port_direction(0, 0x00)
 iobus2.write_port(0, 0x00)
 
 # PARAMETERS
-led_pins = [2, 4, 6, 8]  # van bus 2
+led_pins = [led1, led2, led3, led4]  # van bus 2
 press_time = 2  # seconds
 levels = 6
 
@@ -51,10 +62,10 @@ game_won = multiprocessing.Value('i', 0)
 def flash_all(n):
     """flash all leds for n times"""
     for i in range(0, n):
-        for i in [2, 4, 6, 8]:  # de pins op het IO PI PLUS bord waar de ledjes op zijn aangesloten
+        for i in led_pins:  # de pins op het IO PI PLUS bord waar de ledjes op zijn aangesloten
             iobus2.write_pin(i, 1)
         time.sleep(0.3)
-        for i in [2, 4, 6, 8]:
+        for i in led_pins:
             iobus2.write_pin(i, 0)
         time.sleep(0.3)
     return
@@ -64,33 +75,33 @@ def correct_input(value):
     """check if the input is correct within the time"""
     deadline = time.time() + press_time
     while time.time() < deadline:
-        buttonstate2 = iobus1.read_pin(2)
-        buttonstate4 = iobus1.read_pin(4)
-        buttonstate6 = iobus1.read_pin(6)
-        buttonstate8 = iobus1.read_pin(8)
+        buttonstate1 = iobus1.read_pin(button1)
+        buttonstate2 = iobus1.read_pin(button2)
+        buttonstate3 = iobus1.read_pin(button3)
+        buttonstate4 = iobus1.read_pin(button4)
 
-        if buttonstate2 == 0:
-            ledchoise = 2
+        if buttonstate1 == 0:
+            ledchoise = led1
             print("led1")
-            pygame.mixer.Sound.play(bleep1)
+            mixer.Sound.play(bleep1)
+            break
+
+        elif buttonstate2 == 0:
+            ledchoise = led2
+            print("led2")
+            mixer.Sound.play(bleep2)
+            break
+
+        elif buttonstate3 == 0:
+            ledchoise = led3
+            print("led3")
+            mixer.Sound.play(bleep3)
             break
 
         elif buttonstate4 == 0:
-            ledchoise = 4
-            print("led2")
-            pygame.mixer.Sound.play(bleep2)
-            break
-
-        elif buttonstate6 == 0:
-            ledchoise = 6
-            print("led3")
-            pygame.mixer.Sound.play(bleep3)
-            break
-
-        elif buttonstate8 == 0:
-            ledchoise = 8
+            ledchoise = led4
             print("led4")
-            pygame.mixer.Sound.play(bleep4)
+            mixer.Sound.play(bleep4)
             break
 
     if ledchoise == value:
@@ -113,13 +124,13 @@ def main():
             count += 1
             if count == levels:
                 time.sleep(1)
-                pygame.mixer.Sound.play(good_sound)
+                mixer.Sound.play(good_sound)
                 game_won = True
                 print ("gewonnen")
                 break
             continue
         else:
-            pygame.mixer.Sound.play(wrong_sound)
+            mixer.Sound.play(wrong_sound)
             flash_all(3)
             print("verloren")
         break
