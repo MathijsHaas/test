7bc2c786b09380165875e58e44d3cc924206287a
Mathijs <m.a.hasebos@gmail.com>
2018-09-24 15:24:07 +0200

    socket connectie met sine_test.pde gaande

--- sinus_game.py
+++ sinus_game.py
@@ -1,21 +1,28 @@
 ''' hier moet een soort van socket communicatie gaan plaatsvinden met de processing file. Deze file gedraagt zich als de client waar de sinus processing file zich als server gedraagd. zodra de "server" een start signaal krijgt als de spy_knobs goed staan, gaat het sinus spel van start. zodra het spel is afgerond krijgt deze file een antwoord en is het dus klaar om door te gaan.'''
 import multiprocessing
+import socket
+import time
 
 game_won = multiprocessing.Value('i', 0)
 
 
 def main():
-    host = '127.0.0.1'  # localhost
-    port = 5000  # has to be the same as in the sine_test.pde
+    host = '127.0.0.1'
+    port = 10005
 
     s = socket.socket()
     s.connect((host, port))
 
+    print ("socket connection made and waiting for response")
     message = "start"
     s.send(message.encode('utf-8'))
-    data = s.recv(1024).decode('utf-8')
-    game_won.value = 1  # recv waits until there is an anwser for the server. because there is a reply, it means the game finished.
-    print("sinus game won")
+    data = s.recv(1024)
+    data = data.decode('utf-8')
+    if data == "done":
+        print("received from Sinus processing script: " + data)
+        game_won.value = 1
+        print ("Sinus game_won.value: ", game_won.value)
+    time.sleep(10)  # give blackbox the time to catch up
     s.close()
 
 
