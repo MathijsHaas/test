a4ac99f10318594ca050676f29cd50f8231d8bdf
Mathijs <m.a.hasebos@gmail.com>
2018-09-25 15:59:35 +0200

    ongein die weg mag

--- ongeintestje.py
+++
@@ -1,3 +0,0 @@
-lijst = ["kijk", "devika", "ik", "denk", "zo"]
-
-print (" ".ljust(9 - len()).join(lijst))
--- teststrip.py
+++
@@ -1,8 +0,0 @@
-import time
-import atexit
-
-ding = input("stoppen?: ")
-
-while ding is not 'y':
-    print("doe iets")
-    time.sleep(1)
