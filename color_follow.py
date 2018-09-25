8fbee5e7f75dcfa92305ce83ef040487da7d0d58
Mathijs <m.a.hasebos@gmail.com>
2018-09-25 15:31:51 +0200

    layout.py integrated.

--- color_follow.py
+++ color_follow.py
@@ -1,33 +1,9 @@
 
-from __future__ import absolute_import, division, print_function, \
-    unicode_literals
-try:
-    from IOPi import IOPi
-except ImportError:
-    print("Failed to import IOPi from python system path")
-    print("Importing from parent folder instead")
-    try:
-        import sys
-        sys.path.append("..")
-        from IOPi import IOPi
-    except ImportError:
-        raise ImportError(
-            "Failed to import library from parent folder")
 import time
 import random
 from pygame import mixer
 import layout
 import multiprocessing
-
-button1 = buttonlayout.color_follow_button1
-button2 = buttonlayout.color_follow_button2
-button3 = buttonlayout.color_follow_button3
-button4 = buttonlayout.color_follow_button4
-
-led1 = buttonlayout.color_follow_led1
-led2 = buttonlayout.color_follow_led2
-led3 = buttonlayout.color_follow_led3
-led4 = buttonlayout.color_follow_led4
 
 
 mixer.init()
@@ -38,28 +14,9 @@ bleep4 = mixer.Sound("bleep2.ogg")
 wrong_sound = mixer.Sound("wrong_sound.ogg")
 good_sound = mixer.Sound("good_sound.ogg")
 
-# IO PI PLUS shield setup
-
-iobus1 = IOPi(0x20)  # bus 1 will be inputs
-iobus2 = IOPi(0x21)  # bus 2 will be outputs
-
-# inputs op bus 1
-iobus1.set_port_direction(0, 0xFF)
-iobus1.set_port_pullups(0, 0xFF)
-iobus1.set_port_direction(1, 0xFF)
-iobus1.set_port_pullups(1, 0xFF)
-
-# Outputs op bus 2
-iobus2.set_port_direction(0, 0x00)
-iobus2.write_port(0, 0x00)
-iobus2.set_port_direction(1, 0x00)
-iobus2.write_port(1, 0x00)
 
 # PARAMETERS
-led_pins = [layout.color_follow_led1_value.value,
-            layout.color_follow_led2_value.value,
-            layout.color_follow_led3_value.value,
-            layout.color_follow_led4_value.value]
+led_pins = [led1, led2, led3, led4]
 press_time = 2  # seconds
 levels = 6
 
@@ -69,13 +26,16 @@ game_won = multiprocessing.Value('i', 0)
 def flash_all(n):
     """flash all leds for n times"""
     for i in range(0, n):
-        for i in led_pins:  # de pins op het IO PI PLUS bord waar de ledjes op zijn aangesloten
-            i = 1
+        layout.color_follow_led1_value.value = 1
+        layout.color_follow_led2_value.value = 1
+        layout.color_follow_led3_value.value = 1
+        layout.color_follow_led4_value.value = 1
         time.sleep(0.3)
-        for i in led_pins:
-            i = 0
+        layout.color_follow_led1_value.value = 0
+        layout.color_follow_led2_value.value = 0
+        layout.color_follow_led3_value.value = 0
+        layout.color_follow_led4_value.value = 0
         time.sleep(0.3)
-    return
 
 
 def correct_input(value):
@@ -113,6 +73,28 @@ def correct_input(value):
         return False
 
 
+def put_led_on(led):
+    if led == led1:
+        layout.color_follow_led1_value.value = 1
+    elif led == led2:
+        layout.color_follow_led2_value.value = 1
+    elif led == led3:
+        layout.color_follow_led3_value.value = 1
+    elif led == led4:
+        layout.color_follow_led4_value.value = 1
+
+
+def put_led_off(led):
+    if led == led1:
+        layout.color_follow_led1_value.value = 0
+    elif led == led2:
+        layout.color_follow_led2_value.value = 0
+    elif led == led3:
+        layout.color_follow_led3_value.value = 0
+    elif led == led4:
+        layout.color_follow_led4_value.value = 0
+
+
 def main():
     random.seed()
     count = 0
@@ -120,8 +102,9 @@ def main():
     while True:
         time.sleep(1)
         new_led = random.choice(led_pins)
-        iobus2.write_pin(new_led, 1)  # zet lampje aan
+        put_led_on(new_led)
         status = correct_input(new_led)
+        put_led_off(new_led)
         iobus2.write_pin(new_led, 0)
         if status == True:
             count += 1
