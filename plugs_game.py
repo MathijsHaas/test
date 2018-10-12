526b92c8d5a11de2d95f3a225ef29bb634ce73f1
mathijshaas <m.a.hasebos@gmail.com>
2018-10-12 10:03:48 +0000

    added right values, works on its own

--- plugs_game.py
+++ plugs_game.py
@@ -1,4 +1,5 @@
 from pygame import mixer
+import time
 import layout
 import multiprocessing
 
@@ -8,11 +9,11 @@ good_sound = mixer.Sound("good_sound.ogg")
 
 # --------------------- PARAMETERS -----------------------------------
 
-wait_time = 20  # amount of times it needs to be correct when checked.
+wait_time = 5  # amount of times it needs to be correct when checked.
 margin = 50  # the accepted error
-v1 = 350
-v2 = 350
-v3 = 350
+v1 = 3300
+v2 = 3250
+v3 = 1080
 
 game_won = multiprocessing.Value('i', 0)
 
@@ -22,17 +23,23 @@ game_won = multiprocessing.Value('i', 0)
 def main():
     counter = 0  # so you dont accidentally come past the right voltage
     while True:
+        time.sleep(0.2)
+        print("1: ", layout.plugs1_value.value, "  2: ", layout.plugs2_value.value, "  3: ", layout.plugs3_value.value)
         if (layout.plugs1_value.value >= (v1 - margin) and layout.plugs1_value.value <= (v1 + margin) and
             layout.plugs2_value.value >= (v2 - margin) and layout.plugs2_value.value <= (v2 + margin) and
                 layout.plugs3_value.value >= (v3 - margin) and layout.plugs3_value.value <= (v3 + margin)):
             counter += 1
             if counter == wait_time:
                 mixer.Sound.play(good_sound)
-                game_won is True
-                return True
-            else:
-                counter = 0
+                game_won.value = 1
+                print("plugs rightly plugged")
+                break
+        else:
+            counter = 0
 
 
 if __name__ == "__main__":
+    layout_process = multiprocessing.Process(target=layout.main)
+    layout_process.start()
     main()
+    layout_process.terminate()
